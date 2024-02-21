548021a3a91efd19e73d28e2f8770b11bd3324cf
ahonn <ahonn95@outlook.com>
2024-02-21 16:05:14 +1100

    feat: add mobile nav animation

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -6,8 +6,8 @@ import { NAV_ITEMS } from '../constants';
 import Link from 'next/link';
 import Button from './button';
 import { useState } from 'react';
-import { cn } from '@/lib/utils';
 import { useRouter } from 'next/navigation';
+import { motion } from 'framer-motion';
 
 export type MobileNavProps = {
   dictionary: Record<string, string>;
@@ -26,58 +26,54 @@ export default function MobileNav({ dictionary }: MobileNavProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay />
-        <Dialog.Content
-          className={cn(
-            'fixed top-0 left-0 w-screen h-[400px] backdrop-blur-sm bg-black/75 z-50 py-10 px-12 rounded-b-lg text-white',
-            'transition-transform duration-1000 ease-in-out',
-            isOpen ? 'translate-y-0' : '-translate-y-full',
-          )}
-        >
-          <div className="pb-4 flex justify-between items-center">
-            <Image
-              src="/hamburger-close.svg"
-              alt="Hamburger"
-              width={25}
-              height={16}
-              onClick={() => setIsOpen(false)}
-            />
-            <span className="text-3xl font-bold">
-              <span className="text-[#FF9900]">Bitcoin</span> Singapore
-            </span>
-          </div>
-          <div className="flex justify-between pt-6 border-t border-[#DDE3E3]">
-            <ul>
-              {NAV_ITEMS.map((item) => (
-                <li key={item.label} className="py-1">
-                  <div
-                    className="text-xl"
-                    onClick={() => {
-                      router.replace(item.href);
-                      setIsOpen(false);
-                    }}
-                  >
-                    {dictionary[item.label as keyof typeof dictionary]}
-                  </div>
-                </li>
-              ))}
-            </ul>
-            <div className="flex flex-col gap-2">
-              <span className="text-xl font-bold">{dictionary.cooperation}</span>
-              <Link href="https://t.me/gracegui43" target="_blank" className="flex items-center">
-                <span className="text-xl mr-2">Tetegram</span>
-                <Image src="/open-link.svg" alt="Telegram" width={14} height={14} />
-              </Link>
-              <Link href="mailto:grace@deltalabs.pro" className="flex items-center">
-                <span className="text-xl mr-2">Email</span>
-                <Image src="/open-link.svg" alt="Email" width={14} height={14} />
+        <Dialog.Content className="fixed top-0 left-0 w-screen h-[400px] backdrop-blur-sm bg-black/75 z-50 py-10 px-12 rounded-b-lg text-white">
+          <motion.div initial={{ y: '-100%' }} animate={{ y: 0, transition: { type: 'tween' } }}>
+            <div className="pb-4 flex justify-between items-center">
+              <Image
+                src="/hamburger-close.svg"
+                alt="Hamburger"
+                width={25}
+                height={16}
+                onClick={() => setIsOpen(false)}
+              />
+              <span className="text-3xl font-bold">
+                <span className="text-[#FF9900]">Bitcoin</span> Singapore
+              </span>
+            </div>
+            <div className="flex justify-between pt-6 border-t border-[#DDE3E3]">
+              <ul>
+                {NAV_ITEMS.map((item) => (
+                  <li key={item.label} className="py-1">
+                    <div
+                      className="text-xl"
+                      onClick={() => {
+                        router.replace(item.href);
+                        setIsOpen(false);
+                      }}
+                    >
+                      {dictionary[item.label as keyof typeof dictionary]}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex flex-col gap-2">
+                <span className="text-xl font-bold">{dictionary.cooperation}</span>
+                <Link href="https://t.me/gracegui43" target="_blank" className="flex items-center">
+                  <span className="text-xl mr-2">Tetegram</span>
+                  <Image src="/open-link.svg" alt="Telegram" width={14} height={14} />
+                </Link>
+                <Link href="mailto:grace@deltalabs.pro" className="flex items-center">
+                  <span className="text-xl mr-2">Email</span>
+                  <Image src="/open-link.svg" alt="Email" width={14} height={14} />
+                </Link>
+              </div>
+            </div>
+            <div className="mt-4">
+              <Link href="https://lu.ma/vobk70pd" target="_blank">
+                <Button className="w-full justify-center">{dictionary.register}</Button>
               </Link>
             </div>
-          </div>
-          <div className="mt-4">
-            <Link href="https://lu.ma/vobk70pd" target="_blank">
-              <Button className="w-full justify-center">{dictionary.register}</Button>
-            </Link>
-          </div>
+          </motion.div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
